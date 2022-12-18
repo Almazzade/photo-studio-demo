0dcb253c71a0f25e1b6d03d4bb40a5e190b68503
almazzadeh <almazzadeh@gmail.com>
2022-12-18 23:45:57 +0400

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+    $(".service-carousel").owlCarousel({
         margin: 20,
         dots: false,
         nav: true,
@@ -23,5 +23,25 @@ $(document).ready(function () {
     }, function() {
         $(this).removeClass("deactive active")
         $(this).siblings().removeClass("deactive active");
+    });
+
+    $(".studio-carousel").owlCarousel({
+        dots: false,
+        nav: true,
+        items: 1,
+        navText: [`<img src="assets/images/carousel-prev.svg" class="carouselNavBtn" alt="Prev">`, `<img src="assets/images/carousel-next.svg" class="carouselNavBtn" alt="Next">`],
+        responsive: {
+            0: {
+            },
+            768: {
+                margin: 15,
+                stagePadding: 60
+            },
+            1200: {
+                margin: 120,
+                stagePadding: 180,
+                autoWidth: true
+            }
+        }
     });
 });
